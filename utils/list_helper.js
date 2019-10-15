518cc77d72036f47a748f8965402fa16e89facb2
”Joonas <joonas.kinn@gmail.com>
2019-10-15 22:06:07 +0300

    add tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,27 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+
+const mostLikes = (blogs) => {
+    let blogWithMostLikes = blogs[0]
+    blogs.map(x => x.likes > blogWithMostLikes.likes ? blogWithMostLikes = x : void 0)
+    const result = {
+        "title": blogWithMostLikes.title,
+        "author": blogWithMostLikes.author,
+        "likes": blogWithMostLikes.likes
+    }
+    console.log(result)
+    return result
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    mostLikes
 }
