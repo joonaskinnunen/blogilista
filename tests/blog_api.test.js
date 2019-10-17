5047a080e7d11acf0de8bbb7aa2b1bea4a391f65
”Joonas <joonas.kinn@gmail.com>
2019-10-17 20:00:21 +0300

    add test

---
+++ tests/blog_api.test.js
@@ -0,0 +1,47 @@
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const mongoose = require('mongoose')
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+    },
+    {
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+test('return right amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(2)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
